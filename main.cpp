c3909bf8bb9e85a0f9621d241302cfedb9fe868b
Alexander.A.Kozlov <Alexander.A.Kozlov@kaspersky.com>
2021-01-20 07:42:12 +0300

    Temporary removed boost log

--- main.cpp
+++ main.cpp
@@ -1,11 +1,35 @@
 #include "lib.h"
 
-#include <spdlog/spdlog.h>
+#include "boost/log/trivial.hpp"
+#include "boost/log/utility/setup.hpp"
 
+/*
+static void init_log()
+{
+    static const std::string COMMON_FMT("[%TimeStamp%][%Severity%]:  %Message%");
+
+    boost::log::register_simple_formatter_factory< boost::log::trivial::severity_level, char >("Severity");
+
+    // Output message to console
+    boost::log::add_console_log(
+        std::cout,
+        boost::log::keywords::format = COMMON_FMT,
+        boost::log::keywords::auto_flush = true
+    );
+
+
+    boost::log::add_common_attributes();
+}
+*/
 int main()
 {
-    auto logger = spdlog::stdout_logger_mt("console");
-    logger->info("version {} was started", version());
-
+//    init_log();
+    
+    //auto logger = spdlog::stdout_logger_mt("console");
+    //logger->info("version {} was started", version());
+    
+//    BOOST_LOG_TRIVIAL(info) << "App version " << version() << " was started";
+    std::cout << "App version " << version() << " was started" << std::endl;
+    
     return 0;
 }
